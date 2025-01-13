224411d31acb21899b3d802ef67cccad86b20769
nghiavan7849 <145770259+nghiavan7849@users.noreply.github.com>
2025-01-13 15:48:35 +0700

    Create Class ApiResponse

--- Api_Nhom4_BT2/Program.cs
+++ Api_Nhom4_BT2/Program.cs
@@ -13,6 +13,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options => options.AddDefaultPolicy(policy => 
+    policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()));
 
 var app = builder.Build();
 
